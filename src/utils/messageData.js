b76e4ac9ecab74c291b31dcad2f817712bc36a97
isaiah usiholo <evansaizik@gmail.com>
2023-02-23 14:12:25 +0100

    i worked on the message page and secured it

---
+++ src/utils/messageData.js
@@ -0,0 +1,72 @@
+export const messageData = [
+  {
+    id: 1,
+    photo: null,
+    name: 'Stephen Okon',
+    message: 'Bro how far',
+    time: 'just now'
+  },
+  {
+    id: 2,
+    photo: null,
+    name: 'Black Coder',
+    message: 'The endpoints are working',
+    time: '2 hours ago'
+  },
+  {
+    id: 3,
+    photo: null,
+    name: 'Aizayah',
+    message: 'what\'s up',
+    time: 'yesterday'
+  },
+  {
+    id: 4,
+    photo: null,
+    name: 'Philip',
+    message: 'I\'m going to call you later',
+    time: 'just now'
+  },
+  {
+    id: 5,
+    photo: null,
+    name: 'Justice',
+    message: 'I tried reaching you earlier today, please call me when you get this message',
+    time: 'Monday'
+  },
+  {
+    id: 6,
+    photo: null,
+    name: 'Josiah',
+    message: 'Hello',
+    time: 'Monday'
+  },
+  {
+    id: 7,
+    photo: null,
+    name: 'Samuel Sunday',
+    message: 'Omo, person no dey see you again oh, you just dey chop your money, how far our arrangement this weekend',
+    time: 'just now'
+  },
+  {
+    id: 8,
+    photo: null,
+    name: 'Stephen Okon',
+    message: 'Hello',
+    time: 'an hour ago'
+  },
+  {
+    id: 9,
+    photo: null,
+    name: 'Joshua Ovie',
+    message: 'Good morning, please lets meet on tuesday',
+    time: '6 hours ago'
+  },
+  {
+    id: 10,
+    photo: null,
+    name: 'Sandra Chike',
+    message: 'babe, I\'m not fine',
+    time: 'Friday'
+  },
+]
